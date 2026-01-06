af078aa97e36139c2e1763e12e427e5522ce78b5
PC <shubhamghadi60@gmail.com>
2026-01-06 18:34:21 +0530

    changed sorting to descending in home screen

--- app/src/main/java/com/example/nityaaarti/AartiStorage.kt
+++ app/src/main/java/com/example/nityaaarti/AartiStorage.kt
@@ -7,19 +7,18 @@ object AartiStorage {
     private const val PREF_NAME = "MyAartiPrefs"
     private const val KEY_AARTIS = "saved_aartis"
 
-    // Save an Aarti to the list
     fun addAarti(context: Context, aartiName: String) {
         val prefs = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
         val currentSet = prefs.getStringSet(KEY_AARTIS, mutableSetOf()) ?: mutableSetOf()
         val newSet = HashSet(currentSet)
         newSet.add(aartiName)
-        prefs.edit().putStringSet(KEY_AARTIS, newSet).apply()
+        prefs.edit { putStringSet(KEY_AARTIS, newSet) }
     }
 
     fun getSavedAartis(context: Context): List<String> {
         val prefs = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
         val set = prefs.getStringSet(KEY_AARTIS, emptySet()) ?: emptySet()
-        return set.toList().sorted() // Return sorted list
+        return set.toList().sortedDescending()
     }
 
     fun removeAarti(context: Context, aartiName: String) {
